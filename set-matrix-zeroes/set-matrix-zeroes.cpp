73ae67305ddc0ffc8bd6707410d97efbbd27d885
Anmol Ahuja <ahuja.anmol03@gmail.com>
2022-03-01 09:30:07 +0530

    Update set-matrix-zeroes.cpp

--- set-matrix-zeroes/set-matrix-zeroes.cpp
+++ set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,26 +1,61 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        // T C : O(row*col) = O(n^2)
-        // S C : O(row + col)
-        int row = matrix.size();
-        int col = matrix[0].size();
-        vector<int> rows(row, 1);
-        vector<int> cols(col, 1);
+        /* // Brute
+        int r = matrix.size();
+        int c = matrix[0].size();
+        vector<int> rows(r, 1);
+        vector<int> cols(c, 1);
         
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if(matrix[i][j] == 0) {
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                if (matrix[i][j] == 0) {
                     rows[i] = 0;
                     cols[j] = 0;
+                }
+             }
+        }
+        
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                if(rows[i] == 0 || cols[j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        */
+        
+        //Optimal
+        int r = matrix.size();
+        int c = matrix[0].size();
+        bool flagR = 0, flagC = 0;
+        
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                if (matrix[i][j] == 0) {
+                    if(i == 0) flagR = 1;
+                    if(j == 0) flagC = 1;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
         
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if(rows[i] == 0 || cols[j] == 0) 
+        for(int i = 1; i < r; i++) {
+            for(int j = 1; j < c; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
                     matrix[i][j] = 0;
+            }
+        }
+        
+        if(flagR) {
+            for(int i = 0; i < c; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if(flagC) {
+            for(int i = 0; i < r; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
